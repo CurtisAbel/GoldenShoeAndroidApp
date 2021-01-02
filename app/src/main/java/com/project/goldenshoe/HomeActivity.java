ec3ed9cd5f9b1ac544e16347fca972883f9a9f96
curtis abel <curtislavaud'outlook.com>
2021-01-02 22:58:30 +0000

    added navigation functionalities

--- app/src/main/java/com/project/goldenshoe/HomeActivity.java
+++ app/src/main/java/com/project/goldenshoe/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.project.goldenshoe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -20,12 +25,42 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent home = new Intent(this,HomeActivity.class);
+                this.startActivity(home);
+                        break;
+
+            case R.id.nav_cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CartFragment()).commit();
+                break;
+
+            case R.id.nav_about_us:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutUsFragment()).commit();
+                break;
+
+            case R.id.nav_logout:
+                Intent logout = new Intent(this,MainActivity.class);
+                this.startActivity(logout);
+                break;
+
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -37,4 +72,6 @@ public class HomeActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+
 }
